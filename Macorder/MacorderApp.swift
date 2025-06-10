da1634d99e3c628d0add4f1829de19824b66f015
mmm <kchen00007@gmail.com>
2025-06-09 20:42:42 -0700

    Initial Commit

---
+++ Macorder/MacorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacorderApp.swift
+//  Macorder
+//
+//  Created by Keyu Chen on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MacorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacorderTests/MacorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacorderTests.swift
+//  MacorderTests
+//
+//  Created by Keyu Chen on 6/9/25.
+//
+
+import Testing
+@testable import Macorder
+
+struct MacorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
